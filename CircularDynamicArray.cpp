6d08ea496da9aafa26fff0359ea1af400ce8882e
Taylor Knipe <taylorjknipe@gmail.com>
2024-02-05 17:19:20 -0600

    Implement default and overloaded constructors for dyn. array

--- CircularDynamicArray.cpp
+++ CircularDynamicArray.cpp
@@ -8,4 +8,20 @@ private:
     int size;
     int capacity;
     int frontIndex;
+
+public:
+    CircularDynamicArray()
+    {
+        this->size = 0;
+        this->capacity = 2;
+        this->arr = new T[this->capacity];
+        this->frontIndex = -1;
+    }
+    CircularDynamicArray(int s)
+    {
+        this->size = s;
+        this->capacity = s;
+        this->arr = new T[this->capacity];
+        this->frontIndex = -1;
+    }
 };
